24e3077a604b98490ec9330ed73075a55703c87e
Ricardo Avendaño <43910372+raavendanp@users.noreply.github.com>
2024-11-18 10:13:46 -0500

    Update jenkins_poc_dynamic_nodes.groovy

--- jenkins_poc_dynamic_nodes.groovy
+++ jenkins_poc_dynamic_nodes.groovy
@@ -5,9 +5,7 @@ pipeline {
         stage('Validation') {
             steps {
                 script {
-                    ssh-keyscan github.com github.com >> '/home/dynamicnodes/.ssh/known_hosts'
-                    ssh-keygen -F github.com -f '/home/dynamicnodes/.ssh/known_hosts' # verifies key is correctly installed
-                    ssh -T git@github.com
+                    echo "I am working"
                 }
             }
         }
